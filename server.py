fe5ba83206d98adf90aa1efd8d155dd31fd0277c
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-11-23 23:46:34 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -5,10 +5,15 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = 'index.html'
+        elif self.path == '/write':
+            self.path = 'write.html'
+        elif self.path =='/broadcast':
+            self.path = 'broadcast.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         print(self)
+
         return http.server.SimpleHTTPRequestHandler.do_POST(self)
 # Create an object of the above class
 handler_object = MyHttpRequestHandler
